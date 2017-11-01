dfc047a7b51e8a765376c7b887da29f6165ae712
Shiang-Yu Huang <sky90456@hotmail.com>
2017-11-01 22:56:53 +0800

    Config and push to heroku  v1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp',{
-  useMongoClient: true
-})
+// mongoose.connect('mongodb://localhost:27017/TodoApp',{
+//   useMongoClient: true
+// })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 
 module.exports = {
