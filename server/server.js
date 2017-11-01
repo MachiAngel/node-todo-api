641aca9829c1faf446d620770fdf361d421bb094
Shiang-Yu Huang <sky90456@hotmail.com>
2017-11-01 22:33:30 +0800

    Add GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -29,6 +30,30 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+
+app.get('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send({})
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send({});
+    }
+
+    res.status(200).send({todo});
+
+  }).catch((e) => {
+    res.status(404).send({});
+  })
+
+
+});
+
 
 
 app.listen(3000, () => {
